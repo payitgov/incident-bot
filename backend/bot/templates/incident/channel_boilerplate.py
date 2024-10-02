2c6bd5c60e91482fa7f69b49d601b27487c578b4
Jason Schlepp <jschlepp@payitgov.com>
2024-10-02 10:57:17 -0500

    reverting boilerplate line 44 to what it was before tests started failing

--- backend/bot/templates/incident/channel_boilerplate.py
+++ backend/bot/templates/incident/channel_boilerplate.py
@@ -41,7 +41,7 @@ class IncidentChannelBoilerplateMessage:
                 "type": "section",
                 "text": {
                     "type": "mrkdwn",
-                    "text": "The incident starts out as *declared* mode."
+                    "text": "The incident starts out in *investigating* mode."
                     + " As the incident progresses, it can be moved through "
                     + "statuses until it is resolved.",
                 },
